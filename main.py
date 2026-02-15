754c8fc3675a86d1c24947ab7d8dc9994f9ccfd9
kharris-27 <120191386+k-harris27@users.noreply.github.com>
2026-02-15 22:04:50 +0000

    Improve behaviour when no wifi

--- main.py
+++ main.py
@@ -35,9 +35,13 @@ def check_network_connection():
     It can fail for unexpected reasons (such as state.json not existing). In this case, we can connect manually.
     """
     wlan = network.WLAN(network.WLAN.IF_STA)
-    if not (wlan.active() and wlan.isconnected()):
+    attempted = False
+    while not (wlan.active() and wlan.isconnected()):
         import secrets
+        if attempted:
+            raise RuntimeError(f"Could not connect to wifi network: {secrets.WIFI_SSID}, pass: {secrets.WIFI_PASSWORD}")
         inky_helper.network_connect(secrets.WIFI_SSID, secrets.WIFI_PASSWORD)
+        attempted = True
 
 main()
                 
